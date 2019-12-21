3a0c7260d6fd7c83831dc32db7f57dbe1639cdbd
FranciscoBasantes <fxbasant@espol.edu.ec>
2019-12-20 22:53:19 -0500

    10 acc

---
+++ TallerIntegradorPatrones/src/Patrones/ConcreteAccount5.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Patrones;
+
+import java.util.Currency;
+import java.util.Locale;
+
+public class ConcreteAccount5 extends AccountBuilder {
+
+    @Override
+    public void definirId() {
+        account = new Account(0,0.0);
+        account.setId(12349);
+    }
+
+    @Override
+    public void definirLocale() {
+        account.setLocale(Locale.US);
+    }
+
+    @Override
+    public void definirAmount() {
+       account.setAmount(500.00);
+    }
+
+    @Override
+    public void definirCurrency() {
+        account.setC(Currency.getInstance(Locale.US));
+    }
+
+    
+    
+}
